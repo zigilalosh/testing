bd4dfd66093686ece59f7d88206779c44c59819d
Ziggson <35622555+zigilalosh@users.noreply.github.com>
2025-03-08 15:31:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amazon AWS Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <div id="root"></div>
+    <script src="simple-dashboard.js"></script>
+</body>
+</html>
